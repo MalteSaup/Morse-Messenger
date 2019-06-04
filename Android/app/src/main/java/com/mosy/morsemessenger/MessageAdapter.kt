49a38e7db57d057d6983f45d64d1db75ff9756eb
carolinewolf <27232948+carolinewolf@users.noreply.github.com>
2019-06-04 17:12:42 +0200

    Align Messages left/right

--- Android/app/src/main/java/com/mosy/morsemessenger/MessageAdapter.kt
+++ Android/app/src/main/java/com/mosy/morsemessenger/MessageAdapter.kt
@@ -8,23 +8,28 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RelativeLayout
 import android.widget.TextView
 
 class MessageAdapter(val messageList : ArrayList<Message>) : RecyclerView.Adapter<MessageAdapter.ViewHolder>() {
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val message : Message = messageList[position]
+        val params = holder.textTV?.layoutParams as RelativeLayout.LayoutParams
         holder.textTV?.text = message.text
         holder.textTV?.setPadding(32,24,32,24)
 
-        //change background-image for messages of other chat-member
+        //change background-image for messages of chat-members
         if (message.id == 0) {
             holder.textTV?.setBackgroundResource( R.drawable.messageleft)
             holder.textTV?.setTextColor(ContextCompat.getColor(holder.textTV.context , R.color.colorTextW))
+            params.addRule(RelativeLayout.ALIGN_PARENT_LEFT)
         } else {
             holder.textTV?.setBackgroundResource( R.drawable.messageright)
             holder.textTV?.setTextColor(ContextCompat.getColor(holder.textTV.context , R.color.colorTextB))
+            params.addRule(RelativeLayout.ALIGN_PARENT_RIGHT)
         }
+        holder.textTV.layoutParams = params //causes layout update
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
